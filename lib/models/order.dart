545d88f8c9c46fa5027b555ebea48d7f1be09697
karimrj49 <karimrj49@gmail.com>
2021-11-06 11:47:02 +0100

    1 commit

---
+++ lib/models/order.dart
@@ -0,0 +1,16 @@
+import 'package:burgerapp/models/food.dart';
+import 'package:burgerapp/models/restaurant.dart';
+
+class Order {
+  final Restaurant restaurant;
+  final Food food;
+  final String date;
+  final int quantity;
+
+  Order({
+    required this.date,
+    required this.restaurant,
+    required this.food,
+    required this.quantity,
+  });
+}
